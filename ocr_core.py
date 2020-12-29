1e71b8c554d0f9d536aef064afb27dfa146a5bc1
vovapasko <vovapasko1699@gmail.com>
2020-12-29 16:17:01 +0100

    Provided german language and new image example

--- ocr_core.py
+++ ocr_core.py
@@ -9,5 +9,5 @@ def ocr_core(filename):
     """
     This function will handle the core OCR processing of images.
     """
-    text = pytesseract.image_to_string(Image.open(filename))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+    text = pytesseract.image_to_string(Image.open(filename), lang='deu')  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
     return text  # Then we will print the text in the image
